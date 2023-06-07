b3dd3302db433e7948948975df1b7f5c4ca4557c
Matthew Watson <116296427+MacroEcoMatt@users.noreply.github.com>
2023-06-07 10:40:53 -0400

    Update 08_Linear_Mixed_Effects_Analysis.R

--- 08_Linear_Mixed_Effects_Analysis.R
+++ 08_Linear_Mixed_Effects_Analysis.R
@@ -1,1 +1,13 @@
-
+#' ---
+#' Step 8: Conduct Linear Mixed Effects Modeling
+#' Matthew Watson
+#' conducts LME models for analysis of body size trends
+#' ---
+library(vroom)
+library(dplyr)
+library(ape)
+library(phyr)
+library(sjmisc)
+library(sjPlot)
+library(nlme)
+library(lme4)
