15baa5db5dc286618bbd5acd2c7574af2cc92f29
Matthew Watson <116296427+MacroEcoMatt@users.noreply.github.com>
2023-09-01 12:06:23 -0400

    Update 07_Create_Size_and_Class_files.R

--- 07_Create_Size_and_Class_files.R
+++ 07_Create_Size_and_Class_files.R
@@ -23,25 +23,29 @@ BM_trait <- Bird_Mass %>%left_join(bt)%>%left_join(rl)%>%
   filter(!is.na(lifestyle))%>%filter(!is.na(Migration))%>%
   mutate(activity_cycle = ifelse(is.na(Order), "Diurnal",
                                  ifelse(Order=="Strigiformes", "Nocturnal",
-                                        ifelse(Order=="Caprimulgiformes", "Nocturnal", "Diurnal"))))
+                                        ifelse(Order=="Caprimulgiformes", "Nocturnal", "Diurnal"))),
+        LMass = log10(Mass))
 
 BL_trait <- Bird_Length %>%left_join(bt)%>%left_join(rl)%>%
   filter(!is.na(TPI_month_max))%>%filter(!is.na(AI))%>%filter(!is.na(HLU))%>%
   filter(!is.na(lifestyle))%>%filter(!is.na(Migration))%>%
   mutate(activity_cycle = ifelse(is.na(Order), "Diurnal",
                                  ifelse(Order=="Strigiformes", "Nocturnal",
-                                        ifelse(Order=="Caprimulgiformes", "Nocturnal", "Diurnal"))))
+                                        ifelse(Order=="Caprimulgiformes", "Nocturnal", "Diurnal"))),
+        LLength = log10(Body_Length))
+
 
 BS_trait <- Bird_Size %>%left_join(bt)%>%left_join(rl)%>%
   filter(!is.na(TPI_month_max))%>%filter(!is.na(AI))%>%filter(!is.na(HLU))%>%
   filter(!is.na(lifestyle))%>%filter(!is.na(Migration))%>%
   mutate(activity_cycle = ifelse(is.na(Order), "Diurnal",
                                  ifelse(Order=="Strigiformes", "Nocturnal",
-                                        ifelse(Order=="Caprimulgiformes", "Nocturnal", "Diurnal"))))
+                                        ifelse(Order=="Caprimulgiformes", "Nocturnal", "Diurnal"))),
+        LSize = log10(Mass)/log10(Body_Length))
 
-vroom_write(BM_trait, "D:/Matt/Extra files for process/Bird_Mass.csv", delim = ",")
-vroom_write(BL_trait, "D:/Matt/Extra files for process/Bird_Length.csv", delim = ",")
-vroom_write(BS_trait, "D:/Matt/Extra files for process/Bird_Size.csv", delim = ",")
+vroom_write(BM_trait, "D:/Matt/Extra files for process/Data_S1_Bird_Mass.csv", delim = ",")
+vroom_write(BL_trait, "D:/Matt/Extra files for process/Data_S2_Bird_Length.csv", delim = ",")
+vroom_write(BS_trait, "D:/Matt/Extra files for process/Data_S3_Bird_Size.csv", delim = ",")
 
 ###Create Mammal Data files
 
@@ -56,17 +60,20 @@ iucn_m <- vroom("E:/Coding Files/Chapter 2 Validation/Extra files for process/iu
 M_Mammal <- Mammal_Mass %>%left_join(mamtrait)%>%left_join(iucn)%>%
 filter(!is.na(TPI_month_max))%>%filter(!is.na(AI))%>%filter(!is.na(HLU))%>%
   filter(!lifestyle=="Scansorial")%>%droplevels()%>%
-  filter(!is.na(lifestyle))%>%filter(!is.na(activity_cycle))%>%filter(!is.na(hibernation_torpor))
+  filter(!is.na(lifestyle))%>%filter(!is.na(activity_cycle))%>%filter(!is.na(hibernation_torpor))%>%
+mutate(LMass = log10(Mass))
 
 L_Mammal <- Mammal_Length %>%left_join(mamtrait)%>%left_join(iucn)%>%
   filter(!is.na(TPI_month_max))%>%filter(!is.na(AI))%>%filter(!is.na(HLU))%>%
   filter(!lifestyle=="Scansorial")%>%droplevels()%>%
-  filter(!is.na(lifestyle))%>%filter(!is.na(activity_cycle))%>%filter(!is.na(hibernation_torpor))
+  filter(!is.na(lifestyle))%>%filter(!is.na(activity_cycle))%>%filter(!is.na(hibernation_torpor))%>%
+mutate(LLength = log10(Body_Length))
 
 S_Mammal <- Mammal_Size %>%left_join(mamtrait)%>%left_join(iucn)%>%
   filter(!is.na(TPI_month_max))%>%filter(!is.na(AI))%>%filter(!is.na(HLU))%>%
   filter(!lifestyle=="Scansorial")%>%droplevels()%>%
-  filter(!is.na(lifestyle))%>%filter(!is.na(activity_cycle))%>%filter(!is.na(hibernation_torpor))
+  filter(!is.na(lifestyle))%>%filter(!is.na(activity_cycle))%>%filter(!is.na(hibernation_torpor))%>%
+mutate(LSize = log10(Mass)/log10(Body_Length))
 
 vroom_write(M_Mammal, "D:/Matt/Extra files for process/Mammal_Mass.csv", delim = ",")
 vroom_write(L_Mammal, "D:/Matt/Extra files for process/Mammal_Length.csv", delim = ",")
