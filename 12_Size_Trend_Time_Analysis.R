c61a0d558e17778f07d3d29086fe29fc209b277b
Matthew Watson <116296427+MacroEcoMatt@users.noreply.github.com>
2023-11-13 16:17:26 -0500

    Update 12_Size_Trend_Time_Analysis.R

--- 12_Size_Trend_Time_Analysis.R
+++ 12_Size_Trend_Time_Analysis.R
@@ -48,14 +48,14 @@ b_l_bisqr <- rlm(lm_estimate ~ TPI_trend+AI_trend+HLU_trend,
                  data=length_bird,psi = psi.bisquare)
 
 b_s_bisqr <- rlm(lm_estimate ~ TPI_trend+AI_trend+HLU_trend, 
-               data=size_bird,psi = psi.bisquare)
+                 data=size_bird,psi = psi.bisquare)
 
 tab_model(b_m_bisqr,b_l_bisqr,b_s_bisqr, digits=4)
 r2ww(b_m_bisqr)
 r2ww(b_l_bisqr)
 r2ww(b_s_bisqr)
 
-resid_weights <- data.frame(Binomial = mass_bird$Binomial, resid = b_m_bisqr$resid, weight = bird_all_mass_lm$w)
+resid_weights <- data.frame(Binomial = mass_bird$Binomial, resid = b_m_bisqr$resid, weight = b_m_bisqr$w)
 resid_weigths <- resid_weights[order(b_m_bisqr$w),]
 fitted_val <- fitted(b_m_bisqr)
 Binomial <- mass_bird$Binomial
@@ -65,20 +65,20 @@ plot((resid_weights$resid*resid_weights$weight),resid_weights$fitted_val)
 hist((resid_weights$resid*resid_weights$weight))
 hist(scale((resid_weights$resid*resid_weights$weight)))
 
-resid_l <- data.frame(Binomial = length_bird$Binomial, resid = b_l_bisqr$resid, weight = all_length_lm$w)
+resid_l <- data.frame(Binomial = length_bird$Binomial, resid = b_l_bisqr$resid, weight = b_l_bisqr$w)
 resid_l <- resid_l[order(b_l_bisqr$w),]
 fitted_vall <- fitted(b_l_bisqr)
-Binomial <- length_year$Binomial
+Binomial <- length_bird$Binomial
 cbl <- as.data.frame(cbind(Binomial,fitted_vall))
 resid_l <- left_join(resid_l,cbl)
 plot((resid_l$resid*resid_l$weight),resid_l$fitted_val)
 hist((resid_l$resid*resid_l$weight))
 hist(scale((resid_l$resid*resid_l$weight)))
 
-resid_s <- data.frame(Binomial = size_bird$Binomial, resid = b_s_bisqr$resid, weight = all_size_lmf$w)
+resid_s <- data.frame(Binomial = size_bird$Binomial, resid = b_s_bisqr$resid, weight = b_s_bisqr$w)
 resid_s <- resid_s[order(b_s_bisqr$w),]
 fitted_vals <- fitted(b_s_bisqr)
-Binomial <- size_year$Binomial
+Binomial <- size_bird$Binomial
 cbs <- as.data.frame(cbind(Binomial,fitted_vals))
 resid_s <- left_join(resid_s,cbs)
 plot((resid_s$resid*resid_s$weight),resid_s$fitted_val)
@@ -104,7 +104,7 @@ r2ww(m_s_bisqr)
 resid_weights <- data.frame(Binomial = mass_mam$Binomial, resid = m_m_bisqr$resid, weight = m_m_bisqr$w)
 resid_weigths <- resid_weights[order(m_m_bisqr$w),]
 fitted_val <- fitted(m_m_bisqr)
-Binomial <- mass_bird$Binomial
+Binomial <- mass_mam$Binomial
 cb <- as.data.frame(cbind(Binomial,fitted_val))
 resid_weights <- left_join(resid_weights,cb)
 plot((resid_weights$resid*resid_weights$weight),resid_weights$fitted_val)
@@ -124,7 +124,7 @@ hist(scale((resid_l$resid*resid_l$weight)))
 resid_s <- data.frame(Binomial = size_mam$Binomial, resid = m_s_bisqr$resid, weight = m_s_bisqr$w)
 resid_s <- resid_s[order(m_s_bisqr$w),]
 fitted_vals <- fitted(m_s_bisqr)
-Binomial <- size_year$Binomial
+Binomial <- size_mam$Binomial
 cbs <- as.data.frame(cbind(Binomial,fitted_vals))
 resid_s <- left_join(resid_s,cbs)
 plot((resid_s$resid*resid_s$weight),resid_s$fitted_val)
@@ -150,7 +150,7 @@ bmt_f <- plot(bmt, colors=c("cornflowerblue"), line.size = 1.3)+
   )
 plot(bmt_f)
 
-bma <- ggpredict(b_m_bisqr, terms=c("AI_trend"))
+bma <- ggpredict(b_m_bisqr, terms=c("AI_trend  [-1:1 by=0.01]"))
 bma_f <- plot(bma, colors=c("cornflowerblue"), line.size = 1.3)+
   labs(x = "AI ~ Year (Beta)",
        y = "Body Mass ~ Year (Beta)",
@@ -164,7 +164,7 @@ bma_f <- plot(bma, colors=c("cornflowerblue"), line.size = 1.3)+
   )
 plot(bma_f)
 
-bmh <- ggpredict(b_m_bisqr, terms=c("HLU_trend"))
+bmh <- ggpredict(b_m_bisqr, terms=c("HLU_trend[-0.02:0.02 by=0.001]"))
 bmh_f <- plot(bmh, colors=c("cornflowerblue"), line.size = 1.3)+
   labs(x = "HLU ~ Year (Beta)",
        y = "Body Mass ~ Year (Beta)",
@@ -180,7 +180,7 @@ plot(bmh_f)
 
 #length
 
-blt <- ggpredict(b_l_bisqr, terms=c("TPI_trend"))
+blt <- ggpredict(b_l_bisqr, terms=c("TPI_trend  [-0.1:0.1 by=0.01]"))
 blt_f <- plot(blt, colors=c("cornflowerblue"), line.size = 1.3)+
   labs(x = "TPI ~ Year (Beta)",
        y = "Body Length ~ Year (Beta)",
@@ -194,7 +194,7 @@ blt_f <- plot(blt, colors=c("cornflowerblue"), line.size = 1.3)+
   )
 plot(blt_f)
 
-bla <- ggpredict(b_l_bisqr, terms=c("AI_trend"))
+bla <- ggpredict(b_l_bisqr, terms=c("AI_trend [-0.1:0.1 by=0.01]"))
 bla_f <- plot(bla, colors=c("cornflowerblue"), line.size = 1.3)+
   labs(x = "AI ~ Year (Beta)",
        y = "Body Length ~ Year (Beta)",
@@ -208,7 +208,7 @@ bla_f <- plot(bla, colors=c("cornflowerblue"), line.size = 1.3)+
   )
 plot(bla_f)
 
-blh <- ggpredict(b_l_bisqr, terms=c("HLU_trend"))
+blh <- ggpredict(b_l_bisqr, terms=c("HLU_trend [-0.1:0.1 by=0.01]"))
 blh_f <- plot(blh, colors=c("cornflowerblue"), line.size = 1.3)+
   labs(x = "HLU ~ Year (Beta)",
        y = "Body Length ~ Year (Beta)",
@@ -223,7 +223,7 @@ blh_f <- plot(blh, colors=c("cornflowerblue"), line.size = 1.3)+
 plot(blh_f)
 
 #size
-bst <- ggpredict(b_s_bisqr, terms=c("TPI_trend"))
+bst <- ggpredict(b_s_bisqr, terms=c("TPI_trend [-0.01:0.01 by=0.001]"))
 bst_f <- plot(bst, colors=c("cornflowerblue"), line.size = 1.3)+
   labs(x = "TPI ~ Year (Beta)",
        y = "Mass:Length ~ Year (Beta)",
@@ -237,7 +237,7 @@ bst_f <- plot(bst, colors=c("cornflowerblue"), line.size = 1.3)+
   )
 plot(bst_f)
 
-bsa <- ggpredict(b_s_bisqr, terms=c("AI_trend"))
+bsa <- ggpredict(b_s_bisqr, terms=c("AI_trend [-0.2:0.2 by=0.01]"))
 bsa_f <- plot(bsa, colors=c("cornflowerblue"), line.size = 1.3)+
   labs(x = "AI ~ Year (Beta)",
        y = "Mass:Length ~ Year (Beta)",
@@ -251,7 +251,7 @@ bsa_f <- plot(bsa, colors=c("cornflowerblue"), line.size = 1.3)+
   )
 plot(bsa_f)
 
-bsh <- ggpredict(b_s_bisqr, terms=c("HLU_trend"))
+bsh <- ggpredict(b_s_bisqr, terms=c("HLU_trend [-0.03:0.03 by=0.001]"))
 bsh_f <- plot(bsh, colors=c("cornflowerblue"), line.size = 1.3)+
   labs(x = "HLU ~ Year (Beta)",
        y = "Mass:Length ~ Year (Beta)",
@@ -269,7 +269,7 @@ plot(bsh_f)
 
 
 #mass
-mmt <- ggpredict(m_m_bisqr, terms=c("TPI_trend"))
+mmt <- ggpredict(m_m_bisqr, terms=c("TPI_trend [-0.04:0.04 by=0.01]"))
 mmt_f <- plot(mmt, colors=c("chocolate4"), line.size = 1.3)+
   labs(x = "TPI ~ Year (Beta)",
        y = "Body Mass ~ Year (Beta)",
@@ -283,7 +283,7 @@ mmt_f <- plot(mmt, colors=c("chocolate4"), line.size = 1.3)+
   )
 plot(mmt_f)
 
-mma <- ggpredict(m_m_bisqr, terms=c("AI_trend"))
+mma <- ggpredict(m_m_bisqr, terms=c("AI_trend [-1:1 by=0.01]"))
 mma_f <- plot(mma, colors=c("chocolate4"), line.size = 1.3)+
   labs(x = "AI ~ Year (Beta)",
        y = "Body Mass ~ Year (Beta)",
@@ -297,7 +297,7 @@ mma_f <- plot(mma, colors=c("chocolate4"), line.size = 1.3)+
   )
 plot(mma_f)
 
-mmh <- ggpredict(m_m_bisqr, terms=c("HLU_trend"))
+mmh <- ggpredict(m_m_bisqr, terms=c("HLU_trend [-0.1:0.1 by=0.01]"))
 mmh_f <- plot(mmh, colors=c("chocolate4"), line.size = 1.3)+
   labs(x = "HLU ~ Year (Beta)",
        y = "Body Mass ~ Year (Beta)",
@@ -313,7 +313,7 @@ plot(mmh_f)
 
 #length
 
-mlt <- ggpredict(m_l_bisqr, terms=c("TPI_trend"))
+mlt <- ggpredict(m_l_bisqr, terms=c("TPI_trend [-0.1:0.1 by=0.01]"))
 mlt_f <- plot(mlt, colors=c("chocolate4"), line.size = 1.3)+
   labs(x = "TPI ~ Year (Beta)",
        y = "Body Length ~ Year (Beta)",
@@ -327,7 +327,7 @@ mlt_f <- plot(mlt, colors=c("chocolate4"), line.size = 1.3)+
   )
 plot(mlt_f)
 
-mla <- ggpredict(m_l_bisqr, terms=c("AI_trend"))
+mla <- ggpredict(m_l_bisqr, terms=c("AI_trend [-3:3 by=0.01]"))
 mla_f <- plot(mla, colors=c("chocolate4"), line.size = 1.3)+
   labs(x = "AI ~ Year (Beta)",
        y = "Body Length ~ Year (Beta)",
@@ -356,7 +356,7 @@ mlh_f <- plot(mlh, colors=c("chocolate4"), line.size = 1.3)+
 plot(mlh_f)
 
 #size
-mst <- ggpredict(m_s_bisqr, terms=c("TPI_trend"))
+mst <- ggpredict(m_s_bisqr, terms=c("TPI_trend [-0.05:0.05 by=0.01]"))
 mst_f <- plot(mst, colors=c("chocolate4"), line.size = 1.3)+
   labs(x = "TPI ~ Year (Beta)",
        y = "Mass:Length ~ Year (Beta)",
@@ -370,7 +370,7 @@ mst_f <- plot(mst, colors=c("chocolate4"), line.size = 1.3)+
   )
 plot(mst_f)
 
-msa <- ggpredict(m_s_bisqr, terms=c("AI_trend"))
+msa <- ggpredict(m_s_bisqr, terms=c("AI_trend [-6:6 by=0.01]"))
 msa_f <- plot(msa, colors=c("chocolate4"), line.size = 1.3)+
   labs(x = "AI ~ Year (Beta)",
        y = "Mass:Length ~ Year (Beta)",
@@ -384,7 +384,7 @@ msa_f <- plot(msa, colors=c("chocolate4"), line.size = 1.3)+
   )
 plot(msa_f)
 
-msh <- ggpredict(m_s_bisqr, terms=c("HLU_trend"))
+msh <- ggpredict(m_s_bisqr, terms=c("HLU_trend [-0.1:0.1 by=0.001"))
 msh_f <- plot(msh, colors=c("chocolate4"), line.size = 1.3)+
   labs(x = "HLU ~ Year (Beta)",
        y = "Mass:Length ~ Year (Beta)",
@@ -399,21 +399,21 @@ msh_f <- plot(msh, colors=c("chocolate4"), line.size = 1.3)+
 plot(msh_f)
 
 tpi <- ggpubr::ggarrange(bmt_f, blt_f, bst_f,mmt_f, mlt_f, mst_f,
-                              ncol = 3,
-                              nrow = 2,
-                              labels = "AUTO")
-plot(tpi)
-
-ai <- ggpubr::ggarrange(bma_f, bla_f, bsa_f,mma_f, mla_f, msa_f,
                          ncol = 3,
                          nrow = 2,
                          labels = "AUTO")
-plot(ai)
+plot(tpi)
 
-hlu <- ggpubr::ggarrange(bmh_f, blh_f, bsh_f,mmh_f, mlh_f, msh_f,
+ai <- ggpubr::ggarrange(bma_f, bla_f, bsa_f,mma_f, mla_f, msa_f,
                         ncol = 3,
                         nrow = 2,
                         labels = "AUTO")
+plot(ai)
+
+hlu <- ggpubr::ggarrange(bmh_f, blh_f, bsh_f,mmh_f, mlh_f, msh_f,
+                         ncol = 3,
+                         nrow = 2,
+                         labels = "AUTO")
 plot(hlu)
 
 #####OUTPUT
